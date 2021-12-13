d7644e747a1d1d8d054458c50f4c4bf418eef822
AdmerDev <sjkrantz@aol.com>
2021-12-13 17:16:53 -0600

    Created TMDBMappingService and BasicImageService.

--- MoviePro/Models/Settings/TMDBSettings.cs
+++ MoviePro/Models/Settings/TMDBSettings.cs
@@ -9,7 +9,7 @@ namespace MoviePro.Models.Settings
     {
         public string BaseUrl { get; set; }
         public string BaseImagePath { get; set; }
-        public string BaseYouTubePage { get; set; }
+        public string BaseYouTubePath { get; set; }
         public QueryOptions QueryOptions { get; set; }
     }
 
